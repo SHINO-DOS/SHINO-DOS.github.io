9e211fd5d0febd815be462a21c9ef7418f6b9333
SHINO-DOS <84023646+SHINO-DOS@users.noreply.github.com>
2022-09-24 09:56:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -17,5 +17,5 @@ var coordenadas = () => {
 }
 
 btnyes.addEventListener('click', () => {
-    alert('Ya sabia que querias ir a la iglesia😅😅😅');
+    alert('Ya sabia que querias ser mi novia😅😅😅');
 });
