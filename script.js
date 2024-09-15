1d15d84972b64b272418dcd51ad3e2a0ecbe7d0a
SHINO-DOS <84023646+SHINO-DOS@users.noreply.github.com>
2024-09-14 20:26:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -17,13 +17,5 @@ var coordenadas = () => {
 }
 
 btnyes.addEventListener('click', () => {
-    alert('Sé que a veces la vida puede ser difícil y los estudios pueden ser estresantes, pero no te rindas. Tienes mucho potencial y talento, y estoy seguro de que puedes lograr tus metas si te esfuerzas y perseveras. Quiero animarte con algunas palabras que me motivan a mí:
-
-- "No importa lo lento que vayas, siempre y cuando no te detengas". - Confucio
-- "El éxito no es definitivo, el fracaso no es fatal: lo que cuenta es el coraje para continuar". - Winston Churchill
-- "Nunca dejes de aprender, porque la vida nunca deja de enseñar". - Desconocido
-- "La felicidad no es algo hecho. Proviene de tus propias acciones". - Dalai Lama
-- "Si puedes soñarlo, puedes hacerlo". - Walt Disney
-
-Espero que estas frases te inspiren y te den fuerza para seguir adelante y estudiando. Recuerda que siempre puedes contar conmigo para ayudarte con cualquier duda o consulta que tengas.😊');
+    alert(' Me agrada que seas sincer@, LEE ESTA FRASE: No importa lo lento que vayas, siempre y cuando no te detengas. - Confucio 😊');
 });
