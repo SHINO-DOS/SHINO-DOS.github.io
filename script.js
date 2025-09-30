3041a75fa19632c76510aeb28ed4245339ee6ba4
SHINO-DOS <84023646+SHINO-DOS@users.noreply.github.com>
2025-09-29 22:26:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,22 +2,21 @@ var btnno = document.getElementById('btnno');
 var btnyes = document.getElementById('btnyes');
 
 btnno.addEventListener('mouseenter', () => {
-    console.log("mouse entrando");
-    var pos = coordenadas();
-    btnno.style.top = `${pos.posy}px`;
-    btnno.style.left = `${pos.posx}px`;
-})
+  var pos = coordenadas();
+  btnno.style.top = `${pos.posy}px`;
+  btnno.style.left = `${pos.posx}px`;
+});
 
-var coordenadas = () => {
-    var ancho = screen.width - 100;
-    var alto = screen.height - 200;
-    var posx = Math.random() * (ancho - 0) + 0;
-    var posy = Math.random() * (alto - 0) + 0;
-    return { posx, posy }
+function coordenadas() {
+  // límites dentro del contenedor
+  var ancho = window.innerWidth - 100;
+  var alto = window.innerHeight - 150;
+  var posx = Math.random() * ancho;
+  var posy = Math.random() * alto;
+  return { posx, posy };
 }
 
 btnyes.addEventListener('click', () => {
-    alert('ya sabia que dirias si ja ja :D, tanto te haces rogar ja ja ');
+  alert('¡Ya sabía que dirías que sí! 😍💖');
 });
-
 
